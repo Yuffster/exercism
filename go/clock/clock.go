69928cdf4f5fcbeabfca0cf77309344de7ea96a3
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-07-24 17:51:13 -0400

    Got a clock going on.  Pretty fancy.

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,26 +1,62 @@
-// Clock stub file
-
-// To use the right term, this is the package *clause*.
-// You can document general stuff about the package here if you like.
+// Provides a struct (Clock) which keeps track of a signed integer of
+// minutes.
 package clock
 
-// The value of testVersion here must match `targetTestVersion` in the file
-// clock_test.go.
-const testVersion = 3
+import "fmt"
+import "math"
 
-// Clock API as stub definitions.  No, it doesn't compile yet.
-// More details and hints are in clock_test.go.
+const testVersion = 4
 
-type Clock // Complete the type definition.  Pick a suitable data type.
+// In case our system of keeping time ever changes.
+const hours = 24
+const minutes = 60
+const clockLimit = hours * minutes
 
+// A clock is simply a struct that stores minutes as a base number,
+// then computes the hour and minute values in a 24-hour clock when
+// converted to a string.
+//
+// Internally, minutes can be positive or negative, and can include
+// far longer than the single twenty-four hour cycle present in the
+// final clock value.
+type Clock struct {
+	minutes int
+}
+
+// Creates a new clock based on the hour and minute specified.
+//
+// Hour and minute can be beyond the range of supported clock values
+// and will be normalized.
 func New(hour, minute int) Clock {
+	c := Clock{0}
+	return c.Add(hour*minutes + minute)
 }
 
-func (Clock) String() string {
+// Converts the minutes stored in the clock to a string of
+// HH:MM on a 24-hour clock, zero padded.
+func (c Clock) String() string {
+	m := c.minutes
+	h := 0
+	h = int(math.Floor(float64(m / minutes)))
+	if h == 24 {
+		h = 0
+	}
+	m = m % minutes
+	return fmt.Sprintf("%02d:%02d", h, m)
 }
 
-func (Clock) Add(minutes int) Clock {
+// Adds (or subtracts) the designated number of minutes.
+func (c Clock) Add(minutes int) Clock {
+	c.minutes += minutes
+	// Normalize minutes outside of standard clock range.
+	if c.minutes < 0 {
+		if c.minutes < clockLimit*-1 {
+			c.minutes = clockLimit + (c.minutes % clockLimit)
+		} else {
+			c.minutes = clockLimit + c.minutes
+		}
+	} else if c.minutes > clockLimit {
+		c.minutes = c.minutes % clockLimit
+	}
+	return c
 }
-
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
