7bb4793fab8217a545016632820c8ffe03b0a93a
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-08-25 17:07:50 -0400

    Haskell sublist.

--- haskell/sublist/src/Sublist.hs
+++ haskell/sublist/src/Sublist.hs
@@ -1,6 +1,21 @@
 module Sublist (Sublist(..), sublist) where
 
--- The task is to create the data type `Sublist`, with `Eq` and
--- `Show` instances, and implement the function `sublist`.
+data Sublist = Equal | Sublist | Superlist | Unequal deriving (Show, Eq)
 
-sublist = undefined
+sublist :: Eq a => [a] -> [a] -> Sublist
+sublist [] []      = Equal
+sublist _ []       = Superlist
+sublist [] _       = Sublist
+sublist a b
+    | a == b        = Equal
+    | subsetOf a b  = Sublist
+    | subsetOf b a  = Superlist
+    | otherwise     = Unequal
+
+-- True if a is a subset of b.
+subsetOf :: Eq a => [a] -> [a] -> Bool
+subsetOf a [] = False
+subsetOf a b
+    | length a > length b    = False -- Can't be a superset if it's bigger.
+    | a == take (length a) b = True  -- Could be a prefix.
+    | otherwise              = subsetOf a $ tail b
